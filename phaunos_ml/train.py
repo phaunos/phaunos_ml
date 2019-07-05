34334be8a291379d0b65952918d303a2e4652bc5
Julien Ricard <julien.ricard@gmail.com>
2019-07-05 14:07:48 +0200

    Refactoring

--- phaunos_ml/train.py
+++ phaunos_ml/train.py
@@ -12,37 +12,97 @@ import git
 
 from phaunos_ml.utils.feature_utils import MelSpecExtractor
 from phaunos_ml.utils.dataset_utils import read_dataset_file
-from phaunos_ml.utils.tf_utils import filelist2dataset
+from phaunos_ml.utils.tf_utils import tfrecords2tfdataset
 from phaunos_ml.models import simple_cnn
 
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-sess = tf.Session(config=config)
+def process(config_filename):
+
+    """Training
+
+    Args:
+        config_filename: json config file. Must contain the following key:
+            "feature_path" : path to the TFRecord files (should contain a feature
+                extractor config file named "featex_config.json"
+            "train_set_file" : training dataset file, as defined in phaunos_ml.utils.dataset_utils
+            "n_train_batches" : number of training batches (can be counted by phaunos_ml.utils.dataset_utils.dataset_stat_per_example)
+            "multilabel" : 'true' or 'false'. Whether an example can have multiple labels.
+            "batch_size"
+            "epochs" : number of training epochs
+            "out_dir" : output directory to write log files, git commit #, models...
+            "valid_set_file" (optional) : validation dataset file, as defined in phaunos_ml.utils.dataset_utils
+            "n_valid_batches" (optional) : number of validation batches (can be counted by phaunos_ml.utils.dataset_utils.dataset_stat_per_example)
 
 
-def process(
-        feature_extractor,
-        train_dataset,
-        n_train_batches,
-        out_dir,
-        n_classes,
-        multilabel=False,
-        batch_size=32,
-        epochs=10,
-        valid_dataset=None,
-        n_valid_batches=None
-):
+            
+    """
+
+    with open(config_filename, "r") as config_file:
+        config = json.load(config_file)
+
+    ########################################
+    # create feature extractor from config #
+    ########################################
+
+    try:
+        featex_config = os.path.join(config['feature_path'], 'featex_config.json')
+        feature_extractor = MelSpecExtractor.from_config(featex_config)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f'File {config} not found. Config files must be named "featex_config.json" and located in <feature_path>') from e
+
+    ##############################################
+    # get training and valid (optional) datasets #
+    ##############################################
+
+    train_files, labels = read_dataset_file(
+        config['train_set_file'],
+        prepend_path=config['feature_path'],
+        replace_ext='.tf'
+    )
+    class_list = sorted(list(set.union(*labels)))
+    train_dataset = tfrecords2tfdataset(
+        train_files,
+        feature_extractor.feature_shape,
+        class_list,
+        batch_size=config['batch_size']
+    )
+
+    valid_dataset = None
+    if config['valid_set_file']:
+        valid_files, _ = read_dataset_file(
+            config['valid_set_file'],
+            prepend_path=config['feature_path'],
+            replace_ext='.tf'
+        )
+        valid_dataset = tfrecords2tfdataset(
+            valid_files,
+            feature_extractor.feature_shape,
+            class_list,
+            batch_size=config['batch_size']
+        )
+
+    ##################################
+    # write commit sha in out_dir #
+    ##################################
+    
+    out_dir = os.path.join(config['out_dir'], str(int(time())))
+    pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
+    with open(os.path.join(out_dir, "git_commit_sha.txt"), "w") as f:
+        repo = git.Repo(
+            os.path.dirname(os.path.abspath(__file__)),
+            search_parent_directories=True
+        )
+        f.write(repo.head.object.hexsha)
 
     ###############
     # build model #
     ###############
 
-    h = feature_extractor.example_shape[0]
-    w = feature_extractor.example_shape[1]
+    h = feature_extractor.feature_shape[0]
+    w = feature_extractor.feature_shape[1]
 
-    inputs = keras.Input(shape=(1, h, w), batch_size=batch_size, name='mels')
-    outputs = simple_cnn.build_model(inputs, n_classes, multilabel=multilabel)
+    inputs = keras.Input(shape=(1, h, w), batch_size=config['batch_size'], name='mels')
+    outputs = simple_cnn.build_model(inputs, len(class_list), multilabel=config['multilabel'])
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
     model.summary()
@@ -50,14 +110,14 @@ def process(
     # define optimizer
     optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
 
-    if multilabel:
+    if config['multilabel']:
         loss = keras.losses.binary_crossentropy
     else:
         loss = keras.losses.categorical_crossentropy
 
     model.compile(loss=loss,
                   optimizer=optimizer,
-                  metrics=['accuracy'])
+                  metrics=[tf.keras.metrics.BinaryAccuracy()])
 
 
     ########################
@@ -80,8 +140,8 @@ def process(
     model_dir = os.path.join(out_dir, "models")
     pathlib.Path(model_dir).mkdir(parents=True, exist_ok=True)
     mc = ModelCheckpoint(
-        os.path.join(model_dir, "model.{epoch:02d}-{val_acc:.2f}.h5"),
-        monitor='val_acc',
+        os.path.join(model_dir, "model.{epoch:02d}-{binary_accuracy:.2f}.h5"),
+        monitor='binary_accuracy',
         verbose=1,
         save_best_only=True)
 
@@ -95,92 +155,22 @@ def process(
 
     model.fit(
         train_dataset,
-        steps_per_epoch=n_train_batches,
+        steps_per_epoch=config['n_train_batches'],
         validation_data=valid_dataset,
-        validation_steps=n_valid_batches,
-        epochs=epochs,
+        validation_steps=config.get('n_valid_batches', None),
+        epochs=config['epochs'],
         callbacks=callback_list,
         verbose=2)
 
 
-
 if __name__ == "__main__":
 
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("config_file", type=str)
+    parser.add_argument("config_filename", type=str)
     args = parser.parse_args()
 
-    with open(args.config_file, "r") as config_file:
-        config = json.load(config_file)
-
-
-    ########################################
-    # create feature extractor from config #
-    ########################################
-
-    try:
-        featex_config = os.path.join(config['feature_path'], 'featex_config.json')
-        feature_extractor = MelSpecExtractor.from_config(featex_config)
-    except FileNotFoundError as e:
-        raise FileNotFoundError(f'File {config} not found. Config files must be named "featex_config.json" and located in <feature_path>') from e
-
-    ##############################################
-    # get training and valid (optional) datasets #
-    ##############################################
-
-    train_files, labels = read_dataset_file(
-        config['train_set_file'],
-        prepend_path=config['feature_path'],
-        replace_ext='.tf'
-    )
-    class_list = sorted(list(set.union(*labels)))
-    train_dataset = filelist2dataset(
-        train_files,
-        feature_extractor.example_shape,
-        class_list,
-        batch_size=config['batch_size']
-    )
-
-    valid_dataset = None
-    if config['valid_set_file']:
-        valid_files, _ = read_dataset_file(
-            config['valid_set_file'],
-            prepend_path=config['feature_path'],
-            replace_ext='.tf'
-        )
-        valid_dataset = filelist2dataset(
-            valid_files,
-            feature_extractor.example_shape,
-            class_list,
-            batch_size=config['batch_size']
-        )
-
-    ##################################
-    # write commit sha in out_dir #
-    ##################################
-    
-    out_dir = os.path.join(config['out_dir'], str(int(time())))
-    pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
-    with open(os.path.join(out_dir, "git_commit_sha.txt"), "w") as f:
-        repo = git.Repo(
-            os.path.dirname(os.path.abspath(__file__)),
-            search_parent_directories=True
-        )
-        f.write(repo.head.object.hexsha)
-
-    #########
-    # train #
-    #########
-
-    process(
-        feature_extractor,
-        train_dataset,
-        config['n_train_batches'],
-        out_dir,
-        len(class_list),
-        multilabel=True,
-        valid_dataset=valid_dataset,
-        epochs=config['epochs'],
-        n_valid_batches=config['n_valid_batches'],
-        batch_size=config['batch_size'],
-    )
+    process(args.config_filename)
