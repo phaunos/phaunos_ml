8da1df5a11d4980f5542b22808ad4a7c1909e1ea
Julien Ricard <julien.ricard@gmail.com>
2019-10-23 12:14:53 +0200

    Modify Mixup param so that we can set a max weight on the mixed signal.

--- phaunos_ml/utils/dataug_utils.py
+++ phaunos_ml/utils/dataug_utils.py
@@ -11,26 +11,29 @@ from tensorflow.contrib.image import sparse_image_warp
 Zhang, Hongyi, et al. "mixup: Beyond empirical risk minimization."
 arXiv preprint arXiv:1710.09412 (2017).
 
-Here we add options to
-    - use Uniform distribution instead of the Beta distribution proposed
-    in the paper,
+We modified the original algorithm as follows:
+    - use Uniform distribution instead of the Beta distribution,
     - and to combine one-hot labels using logical OR instead of weighting
 """
 
 class Mixup:
 
-
-    def __init__(self, w_min=0.2):
+    def __init__(self, min_weight=0.0, max_weight=0.4):
         """
         Args:
-            w_min: minimum weight, in [0,1]
+            min_weight, in [0,1]
+            max_weight, in [0,1]
         """
 
-        if w_min < 0 or w_min > 1:
-            raise ValueError('w_min must be in [0,1]')
-        self.dist = tf.distributions.Uniform(low=w_min, high=1-w_min)
+        if (min_weight < 0 or min_weight > 1 or max_weight < 0
+                or max_weight > 1 or min_weight > max_weight):
+            raise ValueError('weights must be in [0,1], with min_weight <= max_weight')
+        self.dist = tf.distributions.Uniform(low=min_weight, high=max_weight)
 
     def process(self, batch1, label1, batch2, label2, batch_size):
+        """Mixup the data.
+        The weight sampled from self.dist is applied to batch2.
+        """
 
         w = tf.cast(self.dist.sample(sample_shape=batch_size), batch1.dtype)
 
@@ -41,7 +44,7 @@ class Mixup:
         w = tf.broadcast_to(w, tf.shape(batch1))
 
         # weighted sum of the features
-        batch = tf.multiply(w,batch1) + tf.multiply(1-w,batch2)
+        batch = tf.multiply(1-w,batch1) + tf.multiply(w,batch2)
 
         # combined labels
         label = tf.logical_or(tf.cast(label1, tf.bool), tf.cast(label2, tf.bool))
